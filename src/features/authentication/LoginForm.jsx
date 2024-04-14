8f69f730dc54421280d996c75948f715670d822a
remon25 <70450564+remon25@users.noreply.github.com>
2024-04-14 02:18:52 +0200

    ddd

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("remon@example.com");
+  const [password, setPassword] = useState("3605412");
   const { login, isLogging } = useLogin();
 
   function handleSubmit(e) {
